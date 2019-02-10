09bf753732fd68225a9550c0fe31769e87fc9a20
Nikita Carulkov <46502210+kytka@users.noreply.github.com>
2019-02-10 10:01:03 +0100

    Zacatek
    Obsahuje pouze enum vsech stavu (dost mozna ani ne uplny).

---
+++ Hra/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum Stav { MENU, VYBERPOCETHRACU, VYBERLEVEL, HRA, VYHRA, PROHRA , PAUZA};
+        Stav stav;
+
+        void NastavStav (Stav jaky)
+        {
+            switch (jaky)
+            {
+                case Stav.MENU:
+                    break;
+                case Stav.VYBERPOCETHRACU:
+                    break;
+                case Stav.VYBERLEVEL:
+                    break;
+                case Stav.HRA:
+                    break;
+                case Stav.VYHRA:
+                    break;
+                case Stav.PROHRA:
+                    break;
+                case Stav.PAUZA:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
